e5cea5c194a5da2478d9a8f6eb1fae6be8add3ba
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2022-12-28 22:28:00 +0300

    Proje dosyası ekle.

---
+++ StatikMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatikMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ogrenci = new
+            {
+                Numara = 35,
+                Ad = "Yasin",
+                Ortalama = 80.5
+            };
+
+            Yaz(ogrenci);
+        }
+        static void Yaz(dynamic ogr)
+        {
+            Console.WriteLine("Öğrencinin adı: "+ogr.Ad);
+        }
+    }
+}
